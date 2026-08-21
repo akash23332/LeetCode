79a60b1ff41d5265fc96c2993e9404611629c0f6
Akash Sharma <akash3102.beai24@chitkara.edu.in>
2026-08-22 00:46:27 +0530

    Time: 18 ms (79.76%), Space: 98 MB (46.19%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -3,23 +3,23 @@ class Solution {
     List<Integer> curr=new ArrayList<>(); 
     public List<List<Integer>> combine(int n, int k) {
 
-        // int[] arr=new int[n];
-        // for(int i=0;i<n;i++){
-        //     arr[i]=i+1;
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=i+1;
 
-        // }
+        }
         
-        comb(n,k,1);
+        comb(n,k,0,arr);
         return ans;
     }
-    public void comb(int n,int k,int start){
+    public void comb(int n,int k,int start,int[] arr){
         if(curr.size()>=k){
             ans.add(new ArrayList<>(curr));
             return;
         }
-        for(int i=start;i<=n;i++){
-            curr.add(i);
-            comb(n,k,i+1);
+        for(int i=start;i<n;i++){
+            curr.add(arr[i]);
+            comb(n,k,i+1,arr);
             curr.remove(curr.size()-1);
         }
 
