8946424b37cb1737938dbebc59ae0d9d0fa74dbe
Akash Sharma <akash3102.beai24@chitkara.edu.in>
2026-09-13 01:05:07 +0530

    Time: 2 ms (69.42%), Space: 44.9 MB (32.37%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,21 +1,20 @@
 class Solution {
     List<String> ans=new ArrayList<>();
     public List<String> generateParenthesis(int n) {
-        para( n ,0,0,"");
+        para(n,"",0,0);
         return ans;
     }
-    public void para(int n ,int close ,int open,String s){
-        if(close==n){
-            ans.add(s);
+    public void para(int n,String curr,int left,int right){
+        if(curr.length()==2*n){
+            ans.add(curr);
             return;
-        }
 
-        if(open<n){
-            para(n,close,open+1,s+"(");
         }
-        if(close<open){
-            para(n,close+1,open,s+")");
+        if(left<n){
+            para(n,curr+"(",left+1,right);
         }
-
+        if(left>right){
+            para(n,curr+")",left,right+1);
+        }
     }
 }
